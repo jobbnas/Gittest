293a15fe803ebbb3613a65ca44ba0001e5dabb17
jonas.okembia@gmail.com <jonas.okembia@gmail.com>
2017-04-11 20:57:20 +0200

    Klar för idag

--- src/gittest2/Gittest2.java
+++ src/gittest2/Gittest2.java
@@ -6,17 +6,24 @@
 package gittest2;
 
 import java.awt.BorderLayout;
+import java.awt.Container;
 import java.awt.FlowLayout;
+import java.awt.GridBagLayout;
+import java.awt.Panel;
+import javafx.scene.paint.Color;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 
 /**
  *
  * @author mcbookpro
  */
-public class Gittest2 extends JFrame {
-
+public class Gittest2 extends JFrame { // extends betyder att Gittest ärver allt som ingår  JFrame. extend ska vi använda till sub/superclasser
+    
     private static final long serialVersionUID = 1L;
 
     /**
@@ -25,18 +32,32 @@ public class Gittest2 extends JFrame {
     public static void main(String[] args) {
         
     new Gittest2().setVisible(true);    
-       
+    
+      
 
 
     }
     private Gittest2(){
-        super("hey");
-        setSize(1000,1000);
+        super("Welcome");
+        setSize(1000,1000); // fönsterstorlek
         setResizable(false);
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setLayout(new FlowLayout());
+        setDefaultCloseOperation(EXIT_ON_CLOSE); // programmet slutar när man trycker på close
+        setLayout(new FlowLayout()); // layout
+        
+        
+        
+        JPanel pnl = new JPanel(); // schysst panel med bild
+        JLabel pnllable = new JLabel(new ImageIcon("/Users/mcbookpro/Downloads/dj-artist-dixon.jpg")); // här kan du lägga till bakrund
+        pnl.setVisible(true); // synlighet
+        pnl.setLocation(500, 500); // panelen placering
+        pnl.setLayout(new GridBagLayout());
+        JButton button2 = new JButton("Test"); //knappar
         JButton button = new JButton("Jk");
-        add(button);
+        button.setBounds(200, 300, 20, 100);
+        add(button); // Lägger till knappar i ramen
+        add(button2);
+        add(pnllable);
+        add(pnl);
         
     }
     
