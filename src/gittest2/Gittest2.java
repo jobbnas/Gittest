373951b06c5b425bfb64de175b87193dba6f29c5
El Rey <El Rey@DESKTOP-R5UNFTE>
2017-04-19 17:27:06 +0200

    adsfassdf

--- src/gittest2/Gittest2.java
+++ src/gittest2/Gittest2.java
@@ -15,8 +15,8 @@ import javax.swing.JFrame;
 
 public class Gittest2{ 
    static final String JDBC_DRIVER = "com.microsoft.sqlserver.jdbc.SQLServerDriver";  
-   static final String DB_URL = "jdbc:sqlserver://myisak.cvhdjaougv6g.eu-central-1.rds.amazonaws.com:1433;databaseName=Github2;integratedSecurity=true;";
-  
+   static final String DB_URL = "jdbc:sqlserver://myisak.cvhdjaougv6g.eu-central-1.rds.amazonaws.com,1433/Github2?user=root&password=199404046253";
+   static final String jdbcUrl = "jdbc:sqlserver://myisak.cvhdjaougv6g.eu-central-1.rds.amazonaws.com:1433/Github2?user=root&password=199404046253";
    static final String USER = "root";
    static final String PASS = "199404046263";
 
@@ -43,31 +43,16 @@ public class Gittest2{
 
       //STEP 3: Open a connection
       System.out.println("Connecting to database...");
-      conn = DriverManager.getConnection(DB_URL,USER,PASS);
+      conn = DriverManager.getConnection(DB_URL);
 
       //STEP 4: Execute a query
       System.out.println("Creating statement...");
-      stmt = conn.createStatement();
-      String sql;
-      sql = "SELECT id, first, last, age FROM Employees";
-      ResultSet rs = stmt.executeQuery(sql);
+    
 
       //STEP 5: Extract data from result set
-      while(rs.next()){
-         //Retrieve by column name
-         int id  = rs.getInt("id");
-         int age = rs.getInt("age");
-         String first = rs.getString("first");
-         String last = rs.getString("last");
-
-         //Display values
-         System.out.print("ID: " + id);
-         System.out.print(", Age: " + age);
-         System.out.print(", First: " + first);
-         System.out.println(", Last: " + last);
-      }
+     
       //STEP 6: Clean-up environment
-      rs.close();
+      
       stmt.close();
       conn.close();
    }catch(SQLException se){
