edd6245494077d7e50812d9db667deff0b919e2e
El Rey <El Rey@DESKTOP-R5UNFTE>
2017-04-19 15:16:50 +0200

    JDBC

--- src/gittest2/Gittest2.java
+++ src/gittest2/Gittest2.java
@@ -5,49 +5,32 @@
  */
 package gittest2;
 
-import java.awt.BorderLayout;
-import java.awt.Container;
-import java.awt.FlowLayout;
-import java.awt.GridBagLayout;
-import java.awt.Panel;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import javafx.scene.paint.Color;
-import javax.swing.ImageIcon;
-import javax.swing.JButton;
-import javax.swing.JDialog;
+
 import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JMenu;
-import javax.swing.JMenuBar;
-import javax.swing.JMenuItem;
-import javax.swing.JOptionPane;
-import javax.swing.JPanel;
 
 
-/**
- *
- * @author mcbookpro
- */
-public class Gittest2 extends JFrame { // extends betyder att Gittest ärver allt som ingår  JFrame. extend ska vi använda till sub/superclasser
+
+
+
+public class Gittest2{ 
+   static final String JDBC_DRIVER = "com.microsoft.sqlserver.jdbc.SQLServerDriver";  
+   static final String DB_URL = "jdbc:sqlserver://localhost;databaseName=JDBC_Test;integratedSecurity=true;";
     
-    private static final long serialVersionUID = 1L;
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         
        
     GUI test = new GUI();
     test.setVisible(true);
-      
-
+    
+    KopplaUpp();
+    
 
     }
+
+    private static void KopplaUpp() {
+       
+    }
    
-   
-    
-    
+  
 }
-/// waddup?
