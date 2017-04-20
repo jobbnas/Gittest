9af5be4f25ee8d3a5b1c0d98005fed03034be462
jonas.okembia@gmail.com <jonas.okembia@gmail.com>
2017-04-20 16:00:35 +0200

    Hejhej

---
+++ src/gittest2/Felsokning.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gittest2;
+
+/**
+ *
+ * @author mcbookpro
+ */
+public class Felsokning {
+    
+    int fel_id;
+    
+}
