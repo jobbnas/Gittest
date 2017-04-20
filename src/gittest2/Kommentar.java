248e731df13983d3aac22b710b00cd2bea197cd2
El Rey <El Rey@DESKTOP-R5UNFTE>
2017-04-20 16:01:48 +0200

    seyrdf

---
+++ src/gittest2/Kommentar.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gittest2;
+
+/**
+ *
+ * @author El Rey
+ */
+public class Kommentar {
+    
+    String comment;
+    String datumstämpel;
+    String underskrift;
+    String kommentar;
+    
+}
