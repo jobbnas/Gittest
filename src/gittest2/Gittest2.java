52b029f7779e650c04770e17d0de38ef46b8ea68
El Rey <El Rey@DESKTOP-R5UNFTE>
2017-04-20 14:22:07 +0200

    Fungerar

--- src/gittest2/Gittest2.java
+++ src/gittest2/Gittest2.java
@@ -15,10 +15,9 @@ import javax.swing.JFrame;
 
 public class Gittest2{ 
    static final String JDBC_DRIVER = "com.microsoft.sqlserver.jdbc.SQLServerDriver";  
-   static final String DB_URL = "jdbc:sqlserver://myisak.cvhdjaougv6g.eu-central-1.rds.amazonaws.com,1433/Github2?user=root&password=199404046253";
-   static final String jdbcUrl = "jdbc:sqlserver://myisak.cvhdjaougv6g.eu-central-1.rds.amazonaws.com:1433/Github2?user=root&password=199404046253";
+   static final String jdbcUrl = "jdbc:sqlserver://myisak.cvhdjaougv6g.eu-central-1.rds.amazonaws.com\\myisak:1433;databaseName=Github2";
    static final String USER = "root";
-   static final String PASS = "199404046263";
+   static final String PASS = "199404046253";
 
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
         
@@ -43,24 +42,33 @@ public class Gittest2{
 
       //STEP 3: Open a connection
       System.out.println("Connecting to database...");
-      conn = DriverManager.getConnection(DB_URL);
+      conn = DriverManager.getConnection(jdbcUrl,USER,PASS);
 
       //STEP 4: Execute a query
       System.out.println("Creating statement...");
-    
-
-      //STEP 5: Extract data from result set
+    stmt = conn.createStatement();
+      String sql;
+      sql = "SELECT * From Kund";
+      
+     ResultSet rs = stmt.executeQuery(sql);
      
-      //STEP 6: Clean-up environment
+           while(rs.next()){
+         //Retrieve by column name
+  
+         String first = rs.getString("fNamn");
+         String last = rs.getString("eNamn");
+        
+          
+         //Display values
+        
+         System.out.println(first + " " +last);
+         
+      }
       
       stmt.close();
       conn.close();
    }catch(SQLException se){
-      //Handle errors for JDBC
-      se.printStackTrace();
    }catch(Exception e){
-      //Handle errors for Class.forName
-      e.printStackTrace();
    }finally{
       //finally block used to close resources
       try{
@@ -72,7 +80,6 @@ public class Gittest2{
          if(conn!=null)
             conn.close();
       }catch(SQLException se){
-         se.printStackTrace();
       }//end finally try
    }//end try
    System.out.println("Goodbye!");
